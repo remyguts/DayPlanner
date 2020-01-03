0e34c365ac239004c8706484eb099302a216a6c1
Remy Guts <remyguts@gmail.com>
2020-01-02 22:55:57 -0800

    created form lines

--- script.js
+++ script.js
@@ -9,4 +9,81 @@ var currentTimecolor;
 var localStorageGet;
 var localStorageSet;
 
+$("div#inputs").append(
+    $("<input/>", {
+        type: 'text',
+        id: 'plan9',
+        name: 'plans',
+        placeholder: ''
+        }
+
+    $("<input/>", {
+            type: 'text',
+            id: 'plan10',
+            name: 'plans',
+            placeholder: ''
+            }
+            $("<input/>", {
+                type: 'text',
+                id: 'plan11',
+                name: 'plans',
+                placehol            }        
+                $("<input/>", {
+                    type: 'text',
+                    id: 'plan12',
+                    name: 'plans',
+                    placeholder: ''
+                    }
+                    $("<input/>", {
+                        type: 'text',
+                        id: 'plan1',
+                        name: 'plans',
+                        placeholder: ''
+                        }
+                        $("<input/>", {
+                            type: 'text',
+                            id: 'plan2',
+                            name: 'plans',
+                            placeholder: ''
+                            }
+                            $("<input/>", {
+                                type: 'text',
+                                id: 'plan3',
+                                name: 'plans',
+                                placeholder: ''
+                                }
+                                $("<input/>", {
+                                    type: 'text',
+                                    id: 'plan4',
+                                    name: 'plans',
+                                    placeholder: ''
+                                    }
+                                    $("<input/>", {
+                                        type: 'text',
+                                        id: 'plan5',
+                                        name: 'plans',
+                                        placeholder: ''
+                                        }
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
