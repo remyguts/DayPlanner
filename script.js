8d697dfa8f658f405388cf68ece7f00f2e5646ad
Remy Guts <remyguts@gmail.com>
2020-01-02 22:32:30 -0800

    creating variables

---
+++ script.js
@@ -0,0 +1,9 @@
+var inputForm;
+var saveBtn;
+var twentyFourHours = moment().format('H');
+var twelveHours = moment().format('h');
+var currentTimecolor;
+var localStorageGet;
+var localStorageSet;
+
+
